7c90d25277f6930fbcc0d94510f25f885afc9077
zet23t <zet23t@googlemail.com>
2015-10-19 22:36:22 +0200

    Initial library commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# TinyDuino 2 Play Lib
+
+This is my 2nd library for the TinyDuino that is specifically targeting
+the TinyScreen+ which is a much more potent processor with more RAM and
+processing power.
+
+This project is still in a very early stage!
+
+## Project structure
+
+* ./srclib: The library files (headers) to be included in the arduino files
+* ./arduino: The arduino projects to test with
+* ./simulator: A simple simulator that enables compilation on PC for testing
+
+For more information, please read the readme files in the respective directories.
---
+++ srclib/lib_StringBuffer.h
@@ -0,0 +1,65 @@
+#ifndef __STRINGBUFFER_H__
+#define STRINGBUFFER_SIZE 100
+
+#ifdef WIN32
+#include <string.h>
+#define strlen_P strlen
+#define strcpy_P strcpy
+#endif // WIN32
+
+class StringBuffer {
+private:
+    char buffer[STRINGBUFFER_SIZE];
+    uint8_t bufferPos;
+    uint8_t currentPos;
+public:
+    void reset() {
+        currentPos = 0;
+        bufferPos = 0;
+        memset(buffer,0,STRINGBUFFER_SIZE);
+    }
+    StringBuffer* start() {
+        currentPos = bufferPos;
+        return this;
+    }
+    StringBuffer* put(const char* str) {
+        while (*str && bufferPos < STRINGBUFFER_SIZE) {
+            buffer[bufferPos++] = *str;
+            str+=1;
+        }
+        return this;
+    }
+    StringBuffer* put(int32_t num) {
+        const char digit[] = "0123456789";
+        char decs[32];
+        uint8_t pos = 0;
+        uint8_t neg = num < 0;
+        if (neg) num = -num;
+        do {
+            uint8_t dec = num%10;
+            num/=10;
+            decs[pos++] = digit[dec];
+        } while (num != 0);
+        if (neg) decs[pos++]= '-';
+        while (pos-- > 0 && bufferPos < STRINGBUFFER_SIZE) buffer[bufferPos++] = decs[pos];
+        return this;
+    }
+    StringBuffer* load(const char *src) {
+        int len = strlen_P(src) + 1;
+		if (STRINGBUFFER_SIZE - bufferPos > len) {
+			char *str = &buffer[bufferPos];
+			bufferPos += len;
+			strcpy_P(str, src);
+		} else {
+		}
+		return this;
+    }
+    char* get() {
+        bufferPos+=1;
+        return &buffer[currentPos];
+    }
+};
+
+StringBuffer stringBuffer;
+
+#endif
