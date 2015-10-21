2dad731fd22169f3ebf0fefac066b2c5488d865f
zet23t <zet23t@googlemail.com>
2015-10-21 21:10:06 +0200

    Testing drawing text to screen

---
+++ arduino/test_text/test_text.ino
@@ -0,0 +1,33 @@
+/**
+ * Visually testing if text output is actually working and not corrupting anything
+ */
+#include <TinyScreen.h>
+#include <SPI.h>
+#include <Wire.h>
+#include "lib_font_virtualdj.h"
+#include "lib_RenderBuffer.h"
+#include "lib_StringBuffer.h"
+
+TinyScreen display = TinyScreen(0);
+RenderBuffer buffer;
+
+void setup() {
+  Wire.begin();
+  SerialUSB.print("hello!");
+  display.begin();
+  display.setFlip(0);
+  display.setBrightness(8);
+  display.setBitDepth(1);
+}
+
+void loop(void) {
+    static unsigned int n = 0;
+    buffer.drawText("hello world",2,-2,RGB565(255,255,255), &virtualDJ_5ptFontInfo);
+    buffer.drawText("hello world",-10,12,RGB565(255,255,255), &virtualDJ_5ptFontInfo);
+    buffer.drawText("hello world",80,22,RGB565(255,255,255), &virtualDJ_5ptFontInfo);
+
+    buffer.drawText("hello world",-20 + ((n++ >> 3) % 128),32,RGB565(255,255,255), &virtualDJ_5ptFontInfo);
+    buffer.drawText("hello world",100 + -((n++ >> 3) % 128),60,RGB565(255,255,255), &virtualDJ_5ptFontInfo);
+
+    buffer.flush(display);
+}
---
+++ simulator/arduino_wrappers/test_text/test_text.cpp
@@ -0,0 +1,8 @@
+/**
+ * Include Arduino main file here to compile it as CPP file.
+ */
+#ifndef PROGMEM
+#define PROGMEM
+#endif // PROGMEM
+
+#include <test_text/test_text.ino>
