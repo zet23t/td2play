fd005d7e033d319fbfafab15a8363e0a3133374f
Eike Decker <zet23t@googlemail.com>
2015-12-17 22:39:23 +0100

    Tilemap rendering class
    This should be framework functionality

---
+++ srclib/lib_tilemap.cpp
@@ -0,0 +1,7 @@
+#include "lib_tilemap.h"
+
+template<class TColor>
+void TileDataMap<TColor>::update(RenderBuffer<TColor>& buffer, const uint16_t cameraX, const uint16_t cameraY) const
+{
+
+}
---
+++ srclib/lib_tilemap.h
@@ -0,0 +1,36 @@
+#ifndef __LIB_TILEMAP_H__
+#define __LIB_TILEMAP_H__
+
+#include <inttypes.h>
+#include "lib_RenderBuffer.h"
+
+template<class TColor>
+class TileDataMap {
+private:
+    // Not sure if packing back- and foreground into same class is wise
+    // could also create tilemap class, but sharing calculations would be
+    // difficult
+    Texture<TColor> backgroundTexture;
+    Texture<TColor> foregroundTexture;
+    uint16_t width, height;
+    const char *backgroundTileMap;
+    const char *foregroundTileMap;
+public:
+    TileDataMap(Texture<TColor> backgroundTexture,
+                Texture<TColor> foregroundTexture,
+                uint16_t width, uint16_t height,
+                const char *backgroundTileMap,
+                const char *foregroundTileMap):
+                    backgroundTexture(backgroundTexture),
+                    foregroundTexture(foregroundTexture),
+                    width(width), height(height),
+                    backgroundTileMap(backgroundTileMap),
+                    foregroundTileMap(foregroundTileMap)
+    {
+
+    }
+
+    void update(RenderBuffer<TColor>& buffer, const uint16_t cameraX, const uint16_t cameraY) const;
+};
+
+#endif // __LIB_TILEMAP_H__
