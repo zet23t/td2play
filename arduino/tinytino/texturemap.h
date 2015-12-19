631750bc34c979357a3804fdad480cb3a8fc97cb
Eike Decker <zet23t@googlemail.com>
2015-12-19 21:42:55 +0100

    Fixed compiler errors

--- arduino/tinytino/texturemap.h
+++ arduino/tinytino/texturemap.h
@@ -1,13 +1,17 @@
 #ifndef __TEXTUREMAP_H__
 #define __TEXTUREMAP_H__
 
+#ifndef PROGMEM
+#define PROGMEM
+#endif // PROGMEM
+#include "lib_renderbuffer.h"
 
 extern const unsigned char _image_ztiles_foreground_data[] PROGMEM;
 extern const unsigned char _image_ztiles_background_data[] PROGMEM;
 
 namespace TextureData {
-    extern const Texture<uint16_t> ztiles_background(_image_ztiles_background_data, TextureType::rgb565progmem, 128, 64, 0);
-    extern const Texture<uint16_t> ztiles_foreground(_image_ztiles_foreground_data, TextureType::rgb565progmem, 128, 64, 0);
+    extern const Texture<uint16_t> ztiles_background;
+    extern const Texture<uint16_t> ztiles_foreground;
 }
 
 #endif // __TEXTUREMAP_H__
