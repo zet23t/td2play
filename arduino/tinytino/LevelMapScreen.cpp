f8227c1047a030b742598dac06ac9c417ac6fb4d
Eike Decker <zet23t@googlemail.com>
2016-01-15 16:42:49 +0100

    basic movement
    collission detection is a problem right now...

--- arduino/tinytino/LevelMapScreen.cpp
+++ arduino/tinytino/LevelMapScreen.cpp
@@ -19,17 +19,47 @@ void LevelMapScreen::init() {
         world.addBody(player);
     }*/
     Body player;
+    player.bounceFactor.setNumber(0,0);
+    player.flyingDrag.setNumber(1,0);
+    player.contactDrag.setNumber(0,0);
     player.id = playerId;
     player.spriteX = 104;
     player.spriteY = 0;
     player.spriteW = 8;
     player.spriteH = 8;
+    player.bodyHandler = this;
     player.position.setXY(100,100);
     player.sprite = TextureData::ztiles_foreground();
     world.addBody(player);
     world.scene = &scene;
     camera.position = Fixed2D4(100,104);
     renderBuffer.setClearBackground(true,renderBuffer.rgb(80,60,70));
+}
+
+void LevelMapScreen::handle(Body *b) {
+    if (b->id == playerId) {
+        uint8_t tileIndex;
+        bool contact = !world.scene->isPixelFree(b->position.x.getIntegerPart()+1, b->position.y.getIntegerPart()+4, tileIndex)
+            || !world.scene->isPixelFree(b->position.x.getIntegerPart()-1, b->position.y.getIntegerPart()+4, tileIndex);
+        if (Joystick::getJoystick().y < 0) {
+            //printf("yo\n");
+            if (contact && jumpCounter > 5) {
+                jumpCounter = 0;
+                b->velocity.y = FixedNumber16<4>(-4,0);
+                b->position.y.setFractionPart(0);
+            }
+        }
+        if (contact) {
+            b->velocity.x = Joystick::getJoystick().x * 4;
+            if (Joystick::getJoystick().x.getFractionPart() != 0)
+                b->velocity.y += FixedNumber16<4>(-1,15);
+        //    renderBuffer.setClearBackground(true,renderBuffer.rgb(80,60,70));
+        } else {
+        //    renderBuffer.setClearBackground(true,renderBuffer.rgb(80,60,120));
+        }
+        camera.position = b->position;
+        jumpCounter+=1;
+    }
 }
 
 void LevelMapScreen::update() {
@@ -56,7 +86,7 @@ void LevelMapScreen::update() {
         (ScreenButtonState::isButtonOn(SCREENBUTTON_TOPRIGHT) ? Fixed2D4(0,8,-1,8) : Fixed2D4(0,0)) +
         (ScreenButtonState::isButtonOn(SCREENBUTTON_TOPLEFT) ? Fixed2D4(-1,8,-1,8) : Fixed2D4(0,0));
 
-    camera.position += offset + Joystick::getJoystick()*8;
+    //camera.position += offset + Joystick::getJoystick()*8;
     renderer.update(renderBuffer, scene, camera.position.x.getIntegerPart(), camera.position.y.getIntegerPart());
     world.updateStep(camera);
     //renderBuffer.drawRect(48, 32,16,16)
@@ -68,7 +98,5 @@ void LevelMapScreen::update() {
     Math::Vector2D16 result = scene.moveOut(pos);
     uint8_t index;
     bool isFree = true; //scene.isPixelFree(pos.x,pos.y,index);
-    renderBuffer.drawRect(result.x - pos.x + 48, result.y - pos.y + 32,1,1)
-                                    ->filledRect(renderBuffer.rgb(isFree ? 255 : 0,90,30));
     //tileMap->update(camera);
 }
--- arduino/tinytino/LevelMapScreen.h
+++ arduino/tinytino/LevelMapScreen.h
@@ -1,23 +1,24 @@
 #include "screen.h"
 #include "physics.h"
 
-class LevelMapScreen : public Screen {
+class LevelMapScreen : public Screen, BodyHandler {
 private:
     TileMap::SceneBgFg<uint16_t> scene;
     TileMap::SceneBgFgRenderer<uint16_t, 200> renderer;
     World world;
     Camera camera;
     const uint8_t playerId = 1;
+    int jumpCounter;
 public:
     LevelMapScreen() {
 
     }
-    LevelMapScreen(TileMap::SceneBgFg<uint16_t> &scene): scene(scene), world(), camera() {
+    LevelMapScreen(TileMap::SceneBgFg<uint16_t> &scene): scene(scene), world(), camera(), jumpCounter(0) {
     }
     void load(TileMap::SceneBgFg<uint16_t>& scene) {
         this->scene = scene;
     }
     void init();
     void update();
-
+    virtual void handle(Body* b);
 };
