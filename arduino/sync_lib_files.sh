086723ff9ab05a2b8c9151f67612d2cf6336fbf9
Eike Decker <zet23t@googlemail.com>
2016-01-18 16:28:24 +0100

    avoid copying lib files into subdirectories of subdirectories

--- arduino/sync_lib_files.sh
+++ arduino/sync_lib_files.sh
@@ -4,7 +4,7 @@ echo "Copying header files from srclib to arduino project directories ..."
 
 cd $(dirname $0)
 
-for D in `find . -type d -name "[^.]*"`
+for D in `find . -type d -name "[^.]*" -maxdepth 1`
 do
   echo "  ... into ${D:2}"
   cp ../srclib/lib_*.* $D
