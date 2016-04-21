e77ac50fd2842c15c034d7539d430b7240f03bd8
Eike Decker <zet23t@googlemail.com>
2016-04-21 22:32:10 +0200

    Adding -= operator to fixed 2d vector and fixed number

--- srclib/lib_FixedMath.h
+++ srclib/lib_FixedMath.h
@@ -106,6 +106,11 @@ public:
         return *this;
     }
 
+    FixedNumber16<shiftNum>& operator -=(const FixedNumber16<shiftNum>& b) {
+        number = ((int16_t)((int32_t)number - (int32_t)b.number));
+        return *this;
+    }
+
     FixedNumber16<shiftNum> operator /(const FixedNumber16<shiftNum>& b) const {
         return FixedNumber16<shiftNum>((int16_t)(((int32_t)number << shiftNum) / (int32_t)b.number));
     }
@@ -214,6 +219,12 @@ public:
     Fixed2D4& operator += (const Fixed2D4& b) {
         x += b.x;
         y += b.y;
+        return *this;
+    }
+
+    Fixed2D4& operator -= (const Fixed2D4& b) {
+        x -= b.x;
+        y -= b.y;
         return *this;
     }
 
