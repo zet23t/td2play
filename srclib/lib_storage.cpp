14692b5fd2d0c364fc0bc4d77665102dcb01ad6c
Eike Decker <zet23t@googlemail.com>
2016-09-07 23:09:44 +0200

    Reworked storage system, partially functional now on win32

--- srclib/lib_storage.cpp
+++ srclib/lib_storage.cpp
@@ -1,18 +1,120 @@
+#include "lib_StringBuffer.h"
+#include "lib_storage.h"
 #ifndef __WIN32__
-
 
 #include <SPI.h>
 #include <SD.h>
 
-namespace Storage {
-    void init(const char *baseDir) {
-    }
-    void write(const char *key, void *data, int size) {
-    }
-    void read(const char *key, void *data, int size) {
+#else
 
-    }
-
-}
+#include <stdio.h>
 
 #endif
+
+namespace Storage {
+    #ifndef __WIN32__
+    class Persistence {
+        bool isInitialized;
+        const char *gBaseDir;
+
+        bool init(const char *baseDir) {
+            gBaseDir = baseDir;
+            isInitialized = true;
+            if (!SD.begin(10)) {
+                isInitialized = false;
+                return false;
+            }
+            return true;
+        }
+        bool write(const void *data, int size) {
+            stringBuffer.start().put(gBaseDir).put('/').put(file);
+            File myFile = SD.open(stringBuffer.getAndForget(), FILE_WRITE);
+            if (myFile) {
+                myFile.write((uint8_t*)data, size);
+                myFile.flush();
+                myFile.close();
+                return true;
+            }
+            return false;
+        }
+        bool read(void *data, int size) {
+            stringBuffer.start().put(gBaseDir).put('/').put(file);
+            File myFile = SD.open(stringBuffer.getAndForget(), FILE_READ);
+            if (myFile) {
+                myFile.read(data, size);
+                myFile.close();
+                return true;
+            }
+            return false;
+        }
+        void close() {
+            isInitialized = false;
+        }
+    };
+    #else
+    class Persistence {
+        FILE *fp;
+    public:
+
+        bool isInitialized;
+        bool init(const char *baseDir) {
+            isInitialized = true;
+
+            fp = fopen(baseDir, "rb+");
+            if (fp == 0) {
+                fp = fopen(baseDir, "wb+");
+            }
+            return fp != 0;
+        }
+        bool write(const void *data, int size) {
+            fwrite(data,size,1,fp);
+            return ferror(fp) == 0;
+        }
+        bool read(void *data, int size) {
+            fread(data, size, 1, fp);
+            return ferror(fp) == 0;
+        }
+        void close() {
+            fclose(fp);
+            fp = 0;
+            isInitialized = false;
+        }
+        void seek(int pos) {
+            fseek(fp,pos,SEEK_SET);
+        }
+    };
+    #endif // __WIN32__
+
+
+
+    Persistence persistence[MAX_PERSISTENCE_COUNT];
+
+
+    Persistence* init(const char *baseDir) {
+        for (int i=0;i<MAX_PERSISTENCE_COUNT;i+=1) {
+            if (persistence[i].isInitialized == false) {
+                persistence[i].init(baseDir);
+                return &persistence[i];
+            }
+        }
+        return 0;
+    }
+
+    bool read(Persistence *p, void *data, int size) {
+        return p->read(data,size);
+    }
+
+    bool write(Persistence *p, const void *data, int size) {
+        return p->write(data, size);
+    }
+
+    bool seek(Persistence *p, int pos) {
+        p->seek(pos);
+    }
+
+    void close(Persistence *p) {
+        p->close();
+    }
+}
+
+
--- srclib/lib_storage.h
+++ srclib/lib_storage.h
@@ -1,7 +1,14 @@
 #ifndef __LIB_STORAGE_H__
+
+#define MAX_PERSISTENCE_COUNT 4
+
 namespace Storage {
-    void init(const char *baseDir);
-    void write(const char *key, void *data, int size);
-    void read(const char *key, void *data, int size);
+    class Persistence;
+
+    Persistence* init(const char *baseDir);
+    bool read(Persistence *p, void *data, int size);
+    bool write(Persistence *p, const void *data, int size);
+    bool seek(Persistence *p, int pos);
+    void close(Persistence *p);
 }
 #endif // __LIB_STORAGE_H__
