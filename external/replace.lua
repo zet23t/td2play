92f91c6fd4ec7c996d04782613051c7ad6e83622
Eike Decker <zet23t@googlemail.com>
2018-01-11 22:28:27 +0100

    improving replacement script not to overwrite when the file content is untouched

--- external/replace.lua
+++ external/replace.lua
@@ -1,8 +1,29 @@
-local seek, repl, file = ...
-local fp = assert(io.open(file,"rb"))
+local seek, repl, from, to = ...
+local fp = assert(io.open(from,"rb"))
 local content = fp:read "*a"
 fp:close()
+
 content = content:gsub(seek,repl)
-fp = assert(io.open(file,"wb"))
+
+local cmpfp = io.open(to, "rb")
+if cmpfp then
+	local cmp = cmpfp:read "*a"
+	if cmp:gsub("%s+"," ") == content:gsub("%s+", " ") then 
+		return
+	end
+	print("Diff", to)
+	local lineA = cmp:gmatch "[^\n]+"
+	local lineB = content:gmatch "[^\n]+"
+	while true do
+		local la = lineA()
+		local lb = lineB()
+		if la ~=lb then print(la.."\n"..lb) end
+		if not la or not lb then break end
+	end
+--	print(cmp,content)
+	cmpfp:close()
+end
+print "updating file"
+fp = assert(io.open(to,"wb"))
 fp:write(content)
 fp:close()
--- external/sync.bat
+++ external/sync.bat
@@ -1,3 +1,2 @@
-copy %~dp0HxCModPlayer\hxcmod.c %~dp0..\srclib\lib_hxcmod.c
 copy %~dp0HxCModPlayer\hxcmod.h %~dp0..\srclib\lib_hxcmod.h
-lua %~dp0replace.lua hxcmod.h lib_hxcmod.h %~dp0..\srclib\lib_hxcmod.c
+lua %~dp0replace.lua hxcmod.h lib_hxcmod.h %~dp0HxCModPlayer\hxcmod.c %~dp0..\srclib\lib_hxcmod.c
