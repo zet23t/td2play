babe56f838c8c0a93210092c4d92238de26736aa
Eike Decker <zet23t@googlemail.com>
2018-01-17 21:50:01 +0100

    typo fix

--- srclib/lib_sound.h
+++ srclib/lib_sound.h
@@ -37,7 +37,7 @@ namespace Sound {
     };
 
     SamplePlayback* playSample(uint16_t id, const int8_t *samples, uint16_t length, uint16_t speed, uint16_t volume, uint16_t loops);
-    void stopSample(int16_t id);
+    void stopSample(uint16_t id);
     void updateVolume(int16_t id, uint16_t volume);
     void updateSpeed(int16_t id, uint16_t speed);
     void init();
