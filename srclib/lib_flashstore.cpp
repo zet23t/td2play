4c974387b6930ca3f4607255558658626c6bfa37
Eike Decker <zet23t@googlemail.com>
2018-06-12 21:15:37 +0200

    non working flash storage solution yet

--- srclib/lib_flashstore.cpp
+++ srclib/lib_flashstore.cpp
@@ -1,15 +1,11 @@
 #include "lib_flashstore.h"
 
-#define FLASHSTORE_START 0xA000
-#define FLASHSTORE_END 0xF000
+#define FLASHSTORE_START 0
+#define FLASHSTORE_END (1024*8)
 #define FLASHSTORE_MAGIC 0xfa3d
 #include "string.h"
 
-#if WIN32
 #include "stdio.h"
-#else
-
-#endif
 
 namespace FlashStore {
 
@@ -103,9 +99,10 @@ namespace FlashStore {
             dataStore[i] = intData[i];
         }
         f = fopen("flash.bin","wb");
-        if (!f) return 0;
+        if (!f) return false;
         fwrite(allData,1,sizeof(allData),f);
         fclose(f);
+        return true;
     }
     bool restoreImpl(uint32_t gameId, uint16_t dataSize, void *result) {
         FILE *f = fopen("flash.bin","rb");
@@ -140,12 +137,168 @@ namespace FlashStore {
 
     #else
 
-    void* findPos(uint32_t gameId, uint16_t dataSize) {
-        void *p = (void*)FLASHSTORE_START;
-        while (p < FLASHSTORE_END) {
 
-        }
+#include <Wire.h>
+#include <SPI.h>
+
+
+const uint32_t pageSizes[] = { 8, 16, 32, 64, 128, 256, 512, 1024 };
+
+#define EEPROM_START ((64-8) * 1024)
+
+struct ReadBufferObject {
+  uint8_t *buffer;
+  uint32_t pos;
+  int32_t size;
+};
+
+uint8_t *buffer;
+
+void flashProgress(const uint32_t, const uint32_t, void*) {
+}
+
+int readBuffer(uint32_t PAGE_SIZE, void *udata) {
+  ReadBufferObject *rbo = (ReadBufferObject*)udata;
+  memcpy(buffer,&rbo->buffer[rbo->pos],PAGE_SIZE);
+  rbo->pos += PAGE_SIZE;
+  return 0;
+}
+
+static void nvmErase(uint32_t erase_dst_addr, uint32_t MAX_FLASH, uint32_t PAGE_SIZE, void (*progress)(const uint32_t, const uint32_t, void*), void* progressData)
+{
+  uint32_t base = erase_dst_addr;
+  while (erase_dst_addr < MAX_FLASH) {
+    // Execute "ER" Erase Row
+    NVMCTRL->ADDR.reg = erase_dst_addr / 2;
+    NVMCTRL->CTRLA.reg = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_ER;
+
+    if (progress) progress(erase_dst_addr - base, MAX_FLASH - base, progressData);
+
+    while (NVMCTRL->INTFLAG.bit.READY == 0) {
+      // intentionally nothing
     }
+    erase_dst_addr += PAGE_SIZE * 4; // Skip a ROW
+  }
+}
+
+static void nvmWrite(uint32_t address, uint32_t fileSize, uint32_t PAGE_SIZE, void (*writeProgress)(const uint32_t, const uint32_t, void*), void* progressData, int (*reader)(uint32_t, void*), void* udata)
+{
+  // Write to flash
+  uint32_t *dst_addr = (uint32_t *)(address);
+  uint32_t size = fileSize;
+
+  // Set automatic page write
+  NVMCTRL->CTRLB.bit.MANW = 0;
+  // Set first address
+  NVMCTRL->ADDR.reg = address / 2;
+  // Do writes in pages
+  while (size) {
+
+    //printCentered("  writing flash  ", 40);
+    reader(PAGE_SIZE, udata);
+
+    if (size < 64) {
+      for (int j = size; j < PAGE_SIZE; j++)buffer[j] = 0xFF;
+    }
+    // Execute "PBC" Page Buffer Clear
+    NVMCTRL->CTRLA.reg = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_PBC;
+    while (NVMCTRL->INTFLAG.bit.READY == 0);
+
+    // Fill page buffer
+    uint32_t i;
+    for (i = 0; i < (PAGE_SIZE / 4) && i < size; i++) {
+      //uint32_t data = (buffer[i + 3] << 0) | (buffer[i + 2] << 1) | (buffer[i + 1] << 2) | (buffer[i] << 3);
+      dst_addr[i] = ((uint32_t *)buffer)[i];
+    }
+
+    // Execute "WP" Write Page
+    NVMCTRL->CTRLA.reg = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_WP;
+
+    writeProgress((uint32_t)dst_addr-address,fileSize,progressData);
+
+    while (NVMCTRL->INTFLAG.bit.READY == 0);
+
+    // Advance to next page
+    dst_addr += i;
+    if (size > PAGE_SIZE)
+      size     -= PAGE_SIZE;
+    else
+      size = 0;
+  }
+}
+
+static bool writeEEPROM(uint8_t *buffer, uint32_t bufferSize, uint32_t offset)
+{
+  if (bufferSize + offset> 8*1024) return false;
+
+  uint32_t address = EEPROM_START + offset;
+
+  uint32_t PAGE_SIZE, PAGES, MAX_FLASH;
+  PAGE_SIZE = pageSizes[NVMCTRL->PARAM.bit.PSZ];
+  PAGES = NVMCTRL->PARAM.bit.NVMP;
+  MAX_FLASH = PAGE_SIZE * PAGES;
+
+  int lastDisplayedValue = -1;
+  uint8_t buf[PAGE_SIZE];
+  buffer = buf;
+
+
+  uint32_t pageAlignedAddress = address & (~(PAGE_SIZE-1));
+  const uint8_t* pagePointer = (const uint8_t*)pageAlignedAddress;
+  uint32_t remainder = address - pageAlignedAddress;
+  if (remainder != 0) {
+    uint8_t page[PAGE_SIZE];
+    memcpy(page,(const void*)pagePointer,PAGE_SIZE);
+    memcpy(page+remainder,(const void*)buffer, PAGE_SIZE-remainder);
+
+    ReadBufferObject rbo = {buffer + PAGE_SIZE-remainder,0,bufferSize - PAGE_SIZE + remainder};
+    ReadBufferObject rbo2 = {page,0,PAGE_SIZE};
+    nvmErase(pageAlignedAddress, address + bufferSize, PAGE_SIZE, flashProgress, 0);
+    nvmWrite(pageAlignedAddress, PAGE_SIZE, PAGE_SIZE, flashProgress, (void*) 1, readBuffer, &rbo2);
+    if (rbo.size > 0) {
+      nvmWrite(pageAlignedAddress, rbo.size, PAGE_SIZE, flashProgress, (void*) 1, readBuffer, &rbo);
+    }
+  } else {
+    ReadBufferObject rbo = {buffer,0,bufferSize};
+    nvmErase(pageAlignedAddress, address + bufferSize, PAGE_SIZE, flashProgress, 0);
+    nvmWrite(pageAlignedAddress, bufferSize, PAGE_SIZE, flashProgress, (void*) 1, readBuffer, &rbo);
+  }
+
+}
+
+static void readEEPROM(uint8_t *buffer, uint32_t size, int32_t offset) {
+  uint32_t address = EEPROM_START + offset;
+  uint32_t *dst_addr = (uint32_t *)(address);
+  memcpy(buffer,(const void*)address,size);
+}
+
+
+
+
+    bool storeImpl(uint32_t gameId, const char* gameName, uint16_t dataSize, void* data) {
+        Chunk *chunk;
+        uint8_t *allData = 0;
+        bool valid = findPosOf(&allData[EEPROM_START], gameId, dataSize, &chunk);
+        if (!valid && chunk == 0) return false;
+
+        uint8_t block[sizeof(Chunk) + dataSize];
+
+        Chunk *chk = (Chunk*)block;
+        chk->init(gameId, gameName, dataSize, data);
+        uint8_t *blockData = &block[sizeof(Chunk)];
+        memcpy(blockData, data, dataSize);
+        writeEEPROM(block, sizeof(block), (uint32_t)chunk - EEPROM_START);
+    }
+
+    bool restoreImpl(uint32_t gameId, uint16_t dataSize, void *result) {
+        uint8_t *allData = 0;
+        Chunk *chunk;
+        bool valid = findPosOf(&allData[EEPROM_START], gameId, dataSize, &chunk);
+        if (!valid && result == 0) return false;
+        result = (void*)&chunk[1];
+        return valid;
+    }
+
 
     #endif // WIN32
 
